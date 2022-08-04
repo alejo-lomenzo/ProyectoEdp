4e3ce70aad043ab7bcf0f0ed3032fa900e5e6b03
Alejo Lo Menzo <alejolomenzo@gmail.com>
2022-08-04 10:48:17 -0300

    Segundo Commit

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULTADO=$( grep -v "\S" $1 | grep -v "\s" | wc -l)
+RESULTADO=$( grep -v "\S" $1 | grep -v "\a" | wc -l)
 
 echo "La cantidad de lineas en blanco en el archivo $1 es de $RESULTADO."
 
